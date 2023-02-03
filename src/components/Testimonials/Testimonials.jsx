c68e42f9650536916d028a68f3f91d4e4a0f5053
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-02-03 13:15:52 -0500

    fixed feedback card addres

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,6 +1,6 @@
 import { feedback } from "../../constants";
 import styles from "../../style";
-import FeedbackCard from "../FeedBackCard/FeedbackCard";
+import FeedbackCard from "../FeedBackCard/FeedBackCard.jsx";
 
 const Testimonials = () => (
   <section
