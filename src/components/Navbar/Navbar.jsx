0f22a8a0436b6498a4d60e25a7bb48838fae270c
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-03-06 18:53:40 -0500

    images compressed and navlinks hovered

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,18 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="hookbank" className="w-[124px] h-[32px]" />
+      <a href="https://bank-app-alpha.vercel.app/">
+        <img
+          src={logo}
+          alt="hookbank"
+          className="w-[124px] h-[32px] cursor-pointer"
+        />
+      </a>
       <ul className="list-none sm:flex hidden justify-end item-center flex-1">
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`fonts-poppins font-normal cursor-pointer text-[16px] 
+            className={`fonts-poppins font-normal cursor-pointer text-[16px] hover:text-secondary 
             ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
