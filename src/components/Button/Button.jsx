b28c8eed9dce619e445f641100401c3e5ca04a5b
Christian Ruiz <christianjavieruizcortes@gmail.com>
2023-03-06 19:29:30 -0500

    pointers and hover added to cards and buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ styles }) => (
   <button
     type="button"
-    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient hover:text-white rounded-[10px] outline-none ${styles}`}
+    className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient hover:bg-cyan-600 rounded-[10px] outline-none ${styles}`}
   >
     Get Started
   </button>
